fef5709e769c812c7ef38a1cceca4abfe15c41f7
Gustavo Henrique <gustavohique12@gmail.com>
2021-11-10 11:43:49 -0300

    criação de divs para manipulação de cada componente de uma tarefa

--- script.js
+++ script.js
@@ -2,37 +2,51 @@
 var i = 0;
 function criaAtividade() {
 
-    if (document.getElementById("atividade").value !== "") {
+    if (document.getElementById("editAtividade").value !== "") {
         i++;
+        var container = document.getElementById("container");
         var div = document.createElement("div");
-        div.id = "atividade " + i;
-        document.body.appendChild(div);
-
+        div.id = "nomeAtividade" + i;
+        container.appendChild(div);
+        
+        var divCheck = document.createElement("div");
+        divCheck.classList.add('divCheck');
+        div.appendChild(divCheck);
         var checkBox = document.createElement("input");
-        checkBox.id = "check " + i;
+        checkBox.id = "check" + i;
         checkBox.type = "checkbox";
         checkBox.name = "checkbox";
-        div.appendChild(checkBox);
+        divCheck.appendChild(checkBox);
+        
 
+        var divSpan = document.createElement("div");
+        divSpan.classList.add("divSpan");
+        div.appendChild(divSpan);
         var span = document.createElement("span");
-        var input = document.querySelector("#atividade");
+        var input = document.querySelector("#editAtividade");
         var texto = input.value;
-        span.id = "span " + i;
-        div.appendChild(span);
-        document.getElementById("span " + i).textContent = texto;
+        span.id = "span" + i;
+        divSpan.appendChild(span);
+        document.getElementById("span" + i).textContent = texto;
+        
 
+        var divExcluir = document.createElement("div");
+        divExcluir.classList.add("divExcluir");
+        div.appendChild(divExcluir);
         var excluir = document.createElement("button");
-        excluir.id = "removeAtividade";
-        excluir.onclick = removeAtividade();
-        div.appendChild(document.createElement("button"));
+        excluir.id = "excluir"  + i;
+       // excluir.onclick = removeAtividade();
+        excluir.innerText = "X";
+        divExcluir.appendChild(excluir);
 
-        document.getElementById("atividade").value = "";
+        document.getElementById("editAtividade").value = "";
     }else{
-        alert('Por favor, preencha o nome da atividade! ');
-        document.getElementById("atividade").focus();
+        alert('Por favor, preencha o nome da tarefa! ');
     }
+    document.getElementById("editAtividade").focus();
 }
 
 function removeAtividade() {
-
+    //remove child
+    //dps remove elemento
 }
