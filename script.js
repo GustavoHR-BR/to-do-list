bbc1f286acc636582cd1a3d36210dfb3c9326f1c
Gustavo Henrique <gustavohique12@gmail.com>
2021-11-10 09:20:55 -0300

    js base

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
-function addAtividade(){
-	
+
+var i = 0;
+function criaAtividade() {
+
+    if (document.getElementById("atividade").value !== "") {
+        i++;
+        var div = document.createElement("div");
+        div.id = "atividade " + i;
+        document.body.appendChild(div);
+
+        var checkBox = document.createElement("input");
+        checkBox.id = "check " + i;
+        checkBox.type = "checkbox";
+        checkBox.name = "checkbox";
+        div.appendChild(checkBox);
+
+        var span = document.createElement("span");
+        var input = document.querySelector("#atividade");
+        var texto = input.value;
+        span.id = "span " + i;
+        div.appendChild(span);
+        document.getElementById("span " + i).textContent = texto;
+
+        var excluir = document.createElement("button");
+        excluir.id = "removeAtividade";
+        excluir.onclick = removeAtividade();
+        div.appendChild(document.createElement("button"));
+
+        document.getElementById("atividade").value = "";
+    }else{
+        alert('Por favor, preencha o nome da atividade! ');
+        document.getElementById("atividade").focus();
+    }
 }
 
-function removeAtividade(){
-	
+function removeAtividade() {
+
 }
