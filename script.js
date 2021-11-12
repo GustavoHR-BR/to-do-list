3eb0585240451c8cea745da760893e06e847a8af
gustavohr <gustavohique12@gmail.com>
2021-11-11 21:38:34 -0300

    exclusão de itens

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ function criaAtividade() {
         i++;
         var container = document.getElementById("container");
         var div = document.createElement("div");
-        div.id = "nomeAtividade" + i;
+        div.id = "atividade" + i;
         container.appendChild(div);
         
         var divCheck = document.createElement("div");
         divCheck.classList.add('divCheck');
         div.appendChild(divCheck);
         var checkBox = document.createElement("input");
-        checkBox.id = "check" + i;
+        checkBox.id = i;
         checkBox.type = "checkbox";
         checkBox.name = "checkbox";
         divCheck.appendChild(checkBox);
@@ -28,15 +28,25 @@ function criaAtividade() {
         span.id = "span" + i;
         divSpan.appendChild(span);
         document.getElementById("span" + i).textContent = texto;
+
+        checkBox.onclick = function(){
+            document.getElementById("span"+this.id).innerText = 
+                document.getElementById("span"+this.id).innerText +
+                 "deuboa";
+        }
         
 
         var divExcluir = document.createElement("div");
         divExcluir.classList.add("divExcluir");
         div.appendChild(divExcluir);
+
         var excluir = document.createElement("button");
-        excluir.id = "excluir"  + i;
-       // excluir.onclick = removeAtividade();
+        excluir.id = i;
         excluir.innerText = "X";
+        excluir.onclick = function()
+        {
+            document.getElementById("atividade"+this.id).remove();
+        }
         divExcluir.appendChild(excluir);
 
         document.getElementById("editAtividade").value = "";
@@ -44,9 +54,4 @@ function criaAtividade() {
         alert('Por favor, preencha o nome da tarefa! ');
     }
     document.getElementById("editAtividade").focus();
-}
-
-function removeAtividade() {
-    //remove child
-    //dps remove elemento
 }
