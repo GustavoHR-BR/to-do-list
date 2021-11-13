1086a39df1d05db2eb74f86ffcb38378f00b6535
gustavohr <gustavohique12@gmail.com>
2021-11-13 11:14:28 -0300

    salvando no local storage

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+var i;
+if ((i = null)) {
+    i = 0;
+}
 
-var i = 0;
 function criaAtividade() {
-
     if (document.getElementById("editAtividade").value !== "") {
         i++;
         var container = document.getElementById("container");
@@ -10,7 +12,7 @@ function criaAtividade() {
         container.appendChild(div);
 
         var divCheck = document.createElement("div");
-        divCheck.classList.add('divCheck');
+        divCheck.classList.add("divCheck");
         div.appendChild(divCheck);
         var checkBox = document.createElement("input");
         checkBox.id = i;
@@ -28,16 +30,17 @@ function criaAtividade() {
         divSpan.appendChild(span);
         document.getElementById("span" + i).textContent = texto;
 
+        localStorage.setItem("nomeAtividade" + i, texto);
+
         var divExcluir = document.createElement("div");
         divExcluir.classList.add("divExcluir");
         div.appendChild(divExcluir);
         var excluir = document.createElement("button");
         excluir.id = i;
-        excluir.style.font.bold;
         excluir.innerText = "X";
         excluir.onclick = function () {
             document.getElementById("atividade" + this.id).remove();
-        }
+        };
         divExcluir.appendChild(excluir);
 
         checkBox.onclick = function () {
@@ -47,11 +50,65 @@ function criaAtividade() {
             } else {
                 nomeAtividade.style.textDecoration = "none";
             }
-        }
+        };
 
+        localStorage.setItem("qtdAtividades", i);
+        localStorage.setItem("indexExcluidos", JSON.stringify(indexExcluidos));
         document.getElementById("editAtividade").value = "";
     } else {
-        alert('Por favor, preencha o nome da tarefa! ');
+        alert("Por favor, preencha o nome da tarefa! ");
     }
     document.getElementById("editAtividade").focus();
+}
+
+function getAtividades() {
+    var qtdAtividades = localStorage.getItem("qtdAtividades");
+    for (var index = 1; index <= qtdAtividades; index++) {
+        var container = document.getElementById("container");
+        var div = document.createElement("div");
+        div.id = "atividade" + index;
+        container.appendChild(div);
+
+        var divCheck = document.createElement("div");
+        divCheck.classList.add("divCheck");
+        div.appendChild(divCheck);
+        var checkBox = document.createElement("input");
+        checkBox.id = index;
+        checkBox.type = "checkbox";
+        checkBox.name = "checkbox";
+        divCheck.appendChild(checkBox);
+
+        var divSpan = document.createElement("div");
+        divSpan.classList.add("divSpan");
+        div.appendChild(divSpan);
+        var span = document.createElement("span");
+        span.id = "span" + index;
+        divSpan.appendChild(span);
+        document.getElementById("span" + index).textContent =
+            localStorage.getItem("nomeAtividade" + index);
+
+        var divExcluir = document.createElement("div");
+        divExcluir.classList.add("divExcluir");
+        div.appendChild(divExcluir);
+        var excluir = document.createElement("button");
+        excluir.id = index;
+        excluir.innerText = "X";
+        excluir.onclick = function () {
+            document.getElementById("atividade" + this.id).remove();
+        };
+        divExcluir.appendChild(excluir);
+
+        checkBox.onclick = function () {
+            var nomeAtividade = document.getElementById("span" + this.id);
+            if (checkBox.checked) {
+                nomeAtividade.style.textDecoration = "line-through";
+            } else {
+                nomeAtividade.style.textDecoration = "none";
+            }
+        };
+
+        document.getElementById("editAtividade").value = "";
+        localStorage.setItem("indexExcluidos", JSON.stringify(indexExcluidos));
+    }
+    i = qtdAtividades;
 }
